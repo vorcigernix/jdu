41555c1ace18b1f91195067d51337658f2b637bd
Adam Sobotka <vorcigernix@gmail.com>
2022-12-30 11:20:26 +0100

    beforelime

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -17,19 +17,18 @@
 	<div class="relative">
 		<span class="absolute inset-y-0 left-0 flex items-center pl-2">
 			<button type="button" title="search" class="p-1 focus:outline-none focus:ring">
-				<svg fill="currentColor" viewBox="0 0 512 512" class="w-4 h-4 text-zinc-100">
-					<path
-						d="M479.6,399.716l-81.084-81.084-62.368-25.767A175.014,175.014,0,0,0,368,192c0-97.047-78.953-176-176-176S16,94.953,16,192,94.953,368,192,368a175.034,175.034,0,0,0,101.619-32.377l25.7,62.2L400.4,478.911a56,56,0,1,0,79.2-79.195ZM48,192c0-79.4,64.6-144,144-144s144,64.6,144,144S271.4,336,192,336,48,271.4,48,192ZM456.971,456.284a24.028,24.028,0,0,1-33.942,0l-76.572-76.572-23.894-57.835L380.4,345.771l76.573,76.572A24.028,24.028,0,0,1,456.971,456.284Z"
-					/>
-				</svg>
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                  </svg>
+                  
 			</button>
 		</span>
 		<input
-			type="search"
+			type="text"
 			name="Search"
 			bind:value={searchTerm}
 			placeholder="Hledej..."
-			class="w-52 py-2 pl-10 text-sm rounded-md sm:w-auto focus:outline-none border-zinc-800 bg-zinc-800 text-zinc-100 focus:bg-zinc-900 focus:border-yellow-400"
+			class="w-52 py-3 pl-10 text-sm rounded-md sm:w-auto bg-zinc-800 border border-transparent outline outline-offset-2 outline-zinc-900 shadow-sm  focus:outline-yellow-300 focus:ring-2 focus:ring-yellow-400"
 		/>
 		{#if searchTerm.length > 1}
 			<div
