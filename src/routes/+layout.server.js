90ee0f083506576cfc626f05f734edc8da20db66
Adam Sobotka <vorcigernix@gmail.com>
2022-12-30 19:53:42 +0100

    remove sw

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -3,5 +3,6 @@ import { functionId } from '$lib/contracts/functionId.js';
 export async function load({ fetch }) {
     const res = await fetch(`https://api.exm.dev/read/${functionId}`);
     const { posts } = await res.json();
+
     return { posts };
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<main class="pt-20 min-h-screen">
+<main class="">
 	<section class="text-gray-400 body-font">
 		<div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
 			<div
@@ -25,40 +25,12 @@
 					>
 				</div>
 			</div>
-			<div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+			<div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 hidden">
 				<img
 					class="object-cover object-center rounded"
 					alt="hero"
 					src="party.svg"
 				/>
-			</div>
-		</div>
-	</section>
-	<section class="body-font hidden">
-		<div
-			class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center  max-w-screen-xl sm:px-6 lg:px-8"
-		>
-			<div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-				<img class="object-cover object-center" alt="hero" src="party.svg" />
-			</div>
-			<div
-				class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center"
-			>
-				<h1 class="title-font sm:text-4xl text-5xl mb-4 font-bold ">
-					Zjisti jestli se <span class="text-lime-500">sejdete</span>!
-				</h1>
-				<p class="mb-8 leading-relaxed">
-					Tahle aplikace slouží k vytvoření opakujících se akcí jako jsou například tréninky.
-					Jakmile založíš akci, ostatní se na ní budou moct přihlásit.
-				</p>
-				<div class="flex w-full md:justify-start justify-center items-end">
-					<a
-						href="/new"
-						class="h-10 inline-flex text-zinc-900 rounded-full bg-lime-500 border-0 py-2 px-6 focus:outline-none hover:bg-lime-600"
-					>
-						<span class="text-xl mr-2 font-bold">* </span>Nová událost</a
-					>
-				</div>
 			</div>
 		</div>
 	</section>
